04865ea63bb4959cc2246bdb7db40487f538874f
VladSG <vladislav.sg@yandex.ru>
2020-11-13 21:41:22 +0300

    fix noexcept, comparison operators, reset(). Add move in destructible_base constructor.

--- optional.h
+++ optional.h
@@ -5,14 +5,14 @@
 
 struct nullopt_t {};
 struct in_place_t {};
-nullopt_t nullopt;
-in_place_t in_place;
+inline nullopt_t nullopt;
+inline in_place_t in_place;
 
 
 namespace optional_ {
     template <typename T, bool v = std::is_trivially_destructible_v<T>>
     struct destructible_base {
-        constexpr destructible_base() {}
+        constexpr destructible_base() noexcept {}
 
         constexpr explicit destructible_base(T value_)
             : value(std::move(value_)),
@@ -24,29 +24,44 @@ namespace optional_ {
                   is_valid(true) {}
 
         ~destructible_base() {
-            if (this->is_valid)
-                this->value.~T();
+            reset();
+        }
+
+        void reset() {
+            if (is_valid) {
+                value.~T();
+                is_valid = false;
+            }
         }
 
     protected:
         union {
             T value;
+            char dummy;
         };
         bool is_valid = false;
     };
 
     template <typename T>
     struct destructible_base<T, true> {
-        constexpr destructible_base() : dummy() {}
+        constexpr destructible_base() noexcept
+            : dummy() {}
 
         explicit constexpr destructible_base(T value_)
-            : value(value_),
+            : value(std::move(value_)),
               is_valid(true) {}
 
         template<typename ...Args>
         constexpr destructible_base(in_place_t, Args&& ...args)
                 : value(std::forward<Args>(args)...),
                   is_valid(true) {}
+
+        void reset() {
+            if (is_valid) {
+                value.~T();
+                is_valid = false;
+            }
+        }
     protected:
         union {
             T value;
@@ -58,6 +73,7 @@ namespace optional_ {
     template <typename T, bool v = std::is_trivially_copyable_v<T>>
     struct optional_base : destructible_base<T> {
         using destructible_base<T>::destructible_base;
+        using destructible_base<T>::reset;
 
         constexpr optional_base(optional_base const& other) {
             if (other.is_valid) {
@@ -66,7 +82,7 @@ namespace optional_ {
             }
         }
 
-        constexpr optional_base(optional_base&& other) {
+        constexpr optional_base(optional_base&& other) noexcept(std::is_nothrow_move_constructible_v<T>) {
             if (other.is_valid) {
                 new(&this->value) T(std::move(other.value));
                 this->is_valid = true;
@@ -82,15 +98,12 @@ namespace optional_ {
                     this->is_valid = true;
                 }
             } else {
-                if (this->is_valid) {
-                    this->value.~T();
-                    this->is_valid = false;
-                }
+                reset();
             }
             return *this;
         }
 
-        optional_base& operator=(optional_base&& other) {
+        optional_base& operator=(optional_base&& other) noexcept(std::is_nothrow_move_constructible_v<T>) {
             if (other.is_valid) {
                 if (this->is_valid) {
                     this->value = std::move(other.value);
@@ -99,10 +112,7 @@ namespace optional_ {
                     this->is_valid = true;
                 }
             } else {
-                if (this->is_valid) {
-                    this->value.~T();
-                    this->is_valid = false;
-                }
+                reset();
             }
             return *this;
         }
@@ -111,6 +121,7 @@ namespace optional_ {
     template <typename T>
     struct optional_base<T, true> : destructible_base<T> {
         using destructible_base<T>::destructible_base;
+        using destructible_base<T>::reset;
     };
 }
 
@@ -121,6 +132,7 @@ class optional : private optional_::optional_base<T> {
     using base = optional_::optional_base<T>;
 public:
     using base::base;
+    using base::reset;
 
     constexpr optional(nullopt_t) noexcept : optional() {};
 
@@ -159,41 +171,70 @@ public:
         new(&this->value) T(std::forward<Args>(args)...);
         this->is_valid = true;
     }
-
-    void reset() {
-        if (this->is_valid) {
-            this->value.~T();
-            this->is_valid = false;
-        }
-    }
 };
 
 template<typename T>
 constexpr bool operator==(optional<T> const &a, optional<T> const &b) {
-    return *a == *b;
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return false;
+    } else if (static_cast<bool>(a)) {
+        return *a == *b;
+    } else {
+        return true;
+    }
 }
 
 template<typename T>
 constexpr bool operator!=(optional<T> const &a, optional<T> const &b) {
-    return *a != *b;
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return true;
+    } else if (static_cast<bool>(a)) {
+        return *a != *b;
+    } else {
+        return false;
+    }
 }
 
 template<typename T>
 constexpr bool operator<(optional<T> const &a, optional<T> const &b) {
-    return (*a) < (*b);
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return static_cast<bool>(b);
+    } else if (static_cast<bool>(a)) {
+        return *a < *b;
+    } else {
+        return false;
+    }
 }
 
 template<typename T>
 constexpr bool operator<=(optional<T> const &a, optional<T> const &b) {
-    return *a <= *b;
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return static_cast<bool>(b);
+    } else if (static_cast<bool>(a)) {
+        return *a <= *b;
+    } else {
+        return true;
+    }
 }
 
 template<typename T>
 constexpr bool operator>(optional<T> const &a, optional<T> const &b) {
-    return *a > *b;
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return static_cast<bool>(a);
+    } else if (static_cast<bool>(a)) {
+        return *a > *b;
+    } else {
+        return false;
+    }
 }
 
 template<typename T>
 constexpr bool operator>=(optional<T> const &a, optional<T> const &b) {
-    return *a >= *b;
+    if (static_cast<bool>(a) != static_cast<bool>(b)) {
+        return static_cast<bool>(a);
+    } else if (static_cast<bool>(a)) {
+        return *a >= *b;
+    } else {
+        return true;
+    }
 }
